6bc0a3b458955b2f4950a3b3cb4f7bca9b7b4b9a
nahuelcaputto <nahuelmartinc@gmail.com>
2021-01-27 16:02:45 -0300

    se agrega seccion de certificaciones y se corrigen temas responsive

---
+++ src/Components/Certifications/certifications.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import data from './certData'
+import './certifications.css'
+
+export default function Certifications() {
+    const certificates = data.certificates;
+    return (
+        <>
+            <div className="container mt-5">
+                <h1 className="mt-5">Certificaciones<strong>.</strong></h1>
+                <div className="row row-cols-1  row-cols-md-2 row-cols-lg-2">
+                    {certificates && certificates.map((cert, i) => (
+                        <div key={i} className="shadow my-1">
+                            <a href={cert.url}><img className="img-fluid" src={cert.urlImage} alt=""></img></a>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+
+    )
+}
