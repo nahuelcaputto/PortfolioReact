d8ed6e89efac29d33b619d5a66412757ecd78e77
nahuelcaputto <nahuelmartinc@gmail.com>
2021-01-27 00:04:00 -0300

    responsive parcial

--- src/Components/LandingPage/landingPage.js
+++ src/Components/LandingPage/landingPage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Grid, Cell } from 'react-mdl';
 import Projects from '../Projects/projects'
 import AboutMe from '../AboutMe/aboutMe'
 import Contact from '../Contact/contact'
@@ -11,40 +10,29 @@ export default function Landing() {
     return (
         <div style={{ width: '100%', margin: 'auto' }}>
             <AtachBar />
-            <Grid className='landing-grid'>
-                <section>
-                    <Element name="Home" />
-                    <Cell col={12}>
-                        <div className="presentation">
-                            <h1>Hola<strong>.</strong></h1>
-                            <div className="presentation-sub">
-                                <p>mi nombre es <strong>Nahuel</strong></p>
-                                <p>soy un <strong>desarrollador web full stack</strong></p>
-                                <p>de <strong>Buenos Aires</strong> para el Mundo</p>
-                            </div>
-                        </div>
-                    </Cell>
-                </section>
-                <section>
-                    <Cell col={12}>
-                        <Element name="About" />
-                        <AboutMe />
-                    </Cell>
-                </section>
-                <section>
-                    <Cell col={12}>
-                        <Element name="Projects" />
-                        <Projects />
-                    </Cell>
-                </section>
-                <section>
-                    <Cell col={12}>
-                        <Element name="Copntact" />
-                        <Contact />
-                    </Cell>
-                </section>
-
-            </Grid>
+            <section >
+                <Element name="Home" />
+                <div className="presentation">
+                    <h1>Hola<strong>.</strong></h1>
+                    <div className="presentation-sub">
+                        <p>mi nombre es <strong>Nahuel</strong></p>
+                        <p>soy un <strong>desarrollador web full stack</strong></p>
+                        <p>de <strong>Buenos Aires</strong> para el Mundo</p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <Element name="About" />
+                <AboutMe />
+            </section>
+            <section className="projects-section">
+                <Element name="Projects" />
+                <Projects />
+            </section>
+            <section>
+                <Element name="Copntact" />
+                <Contact />
+            </section>
         </div>
     )
 }
--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 import "./projects.css";
 
 export default function Projects() {
@@ -8,24 +7,28 @@ export default function Projects() {
     return (
         <>
             <h1 className='title'>proyectos<strong>.</strong></h1>
-            <div className="projects-section">
+            <div className='container m-10'>
+                <div className='row row-cols-1  row-cols-md-3 row-cols-lg-4 '>
+                    {projects && projects.map((project, i) => (
 
-                {projects && projects.map((project, i) => (
-                    <Card key={i} shadow={5} className="card">
-                        <CardTitle style={{ color: '#FFF', textShadow: '2px 2px 5px #ff4081', fontWeight: 'bold', fontSize: '25px', height: '176px', alignItems: 'flex-end', background: `url(${project.urlImage}) center / cover` }}> {project.name}</CardTitle>
-                        <CardText className="card-text">{project.descriptionES}</CardText>
-                        <CardActions border className="card-actions">
-                            {project.github ? (<Button style={{ color: '#ff4081' }} colored href={project.github}>Github</Button>) : (<></>)}
-                            {project.urlPage ? (<Button colored href={project.urlPage}>Deploy</Button>) : (<></>)}
-                        </CardActions>
-                        <CardMenu style={{ color: '#fff' }}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
+                        <div class="col m-2 project-card" key={i}>
+                            <div class="card">
+                                <img src={project.urlImage} class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <h5 class="card-title">{project.name}</h5>
+                                    <p class="card-text">{project.descriptionES}</p>
+                                    <div className='d-flex justify-content-between'>
+                                        {project.github ? (<a href={project.github} class="btn btn-outline-primary mx-1">Github</a>) : (<></>)}
+                                        {project.urlPage ? (<a href={project.urlPage} class="btn btn-outline-primary mx-1">Deploy</a>) : (<></>)}
+                                    </div>
 
-                ))
-                }
-            </div >
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                    }
+                </div>
+            </div>
         </>
     )
 }
